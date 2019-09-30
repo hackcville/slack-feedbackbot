c34cae0bfd05741b3b3085bfed3e98524f930dd0
camillemcooper <48187933+camillemcooper@users.noreply.github.com>
2019-09-30 14:23:05 -0400

    reworked call to Airtable to reflect updated data structure

--- course_roster.js
+++ course_roster.js
@@ -14,38 +14,35 @@ const AIRTABLE_BASE_ID = process.env.AIRTABLE_BASE_ID_FALL_INVOLVEMENT_ID;
 var Airtable = require("airtable");
 var base = new Airtable({ apipKey: AIRTABLE_API_KEY }).base(AIRTABLE_BASE_ID);
 
-idk(base);
-
-async function idk(base) {
-  var course_info = await getCourses(base);
-  var student_info = await getStudents(base);
-  console.log(course_info);
-  console.log(student_info);
-}
-
-// Promise.all([course_info, student_info])
-//   .then(function(results) {
-//     console.log(results);
-//     console.log("all data has loaded");
-//   })
-//   .catch(function(err) {
-//     console.log("one or more promises has failed: " + err);
-//   });
+getCourses(base);
 
 async function getCourses(base) {
   var courses = [];
   await base("Courses")
     .select({
-      fields: ["Courses", "Section", "Day", "Meeting Time"]
+      fields: [
+        "Course Title",
+        "Enrolled Students",
+        "Slack ID",
+        "Day",
+        "Meeting Time"
+      ]
     })
     .eachPage(
       function page(records, fetchNextPage) {
         records.forEach(function(record) {
-          let course_section =
-            record.get("Courses") + " " + record.get("Section");
+          let course = record.get("Course Title");
+          let students = record.get("Enrolled Students");
+          let slacks = record.get("Slack ID");
           let meeting_time =
             record.get("Day") + " " + record.get("Meeting Time");
-          courses[course_section] = { time: meeting_time, students: [] };
+          let entry = {
+            course: course,
+            student_names: students,
+            slack_ids: slacks,
+            time: meeting_time
+          };
+          courses.push(entry);
         });
         fetchNextPage();
       },
@@ -54,36 +51,37 @@ async function getCourses(base) {
           console.error(err);
           return;
         } else {
+          console.log(courses);
           return courses;
         }
       }
     );
 }
 
-async function getStudents(base) {
-  var students = {};
-  await base("Fall 2019 Involvement")
-    .select({
-      fields: ["Name", "Slack", "Fall Course Involvement - Section"],
-      filterByFormula:
-        "AND(NOT({Fall Course Involvement - Section} = 'Dropped Fall 2019'),NOT({Fall Course Involvement - Section} = ''))"
-    })
-    .eachPage(
-      function page(records, fetchNextPage) {
-        records.forEach(function(record) {
-          let course = record.get("Fall Course Involvement - Section");
-          let slack = record.get("Slack");
-          let student = record.get("Name");
-          students[student] = { slack: slack, course: course };
-        });
-        fetchNextPage();
-      },
-      function done(err) {
-        if (err) {
-          console.error(err);
-          return;
-        }
-        return students;
-      }
-    );
-}
+// async function getStudents(base) {
+//   var students = {};
+//   await base("Fall 2019 Involvement")
+//     .select({
+//       fields: ["Name", "Slack", "Fall Course Involvement - Section"],
+//       filterByFormula:
+//         "AND(NOT({Fall Course Involvement - Section} = 'Dropped Fall 2019'),NOT({Fall Course Involvement - Section} = ''))"
+//     })
+//     .eachPage(
+//       function page(records, fetchNextPage) {
+//         records.forEach(function(record) {
+//           let course = record.get("Fall Course Involvement - Section");
+//           let slack = record.get("Slack");
+//           let student = record.get("Name");
+//           students[student] = { slack: slack, course: course };
+//         });
+//         fetchNextPage();
+//       },
+//       function done(err) {
+//         if (err) {
+//           console.error(err);
+//           return;
+//         }
+//         return students;
+//       }
+//     );
+// }
