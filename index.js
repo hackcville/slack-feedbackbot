75899fc9ecc9c7270546769a5e383b1bffa923c5
camillemcooper <48187933+camillemcooper@users.noreply.github.com>
2019-10-14 14:15:36 -0400

    added the correct questions that upon submit will record in the Fall feedback table in airtable

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-/* 
+/*
  * index.js
  * Server to recieve events/actions from Slack and send surveys and record responses
- * by Forrest Feaser
+ * by Forrest Feaser and Camille Cooper
  * for HackCville, Inc.
  * 9/12/2019
  */
 
-require('dotenv').config();
+require("dotenv").config();
 
 const SLACK_BOT_TOKEN = process.env.SLACK_BOT_TOKEN;
 const SLACK_SIGNING_SECRET = process.env.SLACK_SIGNING_SECRET;
 const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY;
-const AIRTABLE_BASE_ID_DEV = process.env.AIRTABLE_BASE_ID_DEV;
+const AIRTABLE_BASE_ID = process.env.AIRTABLE_BASE_ID_LIVE;
 
-var Airtable = require('airtable');
-var base = new Airtable({apiKey: AIRTABLE_API_KEY}).base(AIRTABLE_BASE_ID_DEV);
+var Airtable = require("airtable");
+var base = new Airtable({ apiKey: AIRTABLE_API_KEY }).base(AIRTABLE_BASE_ID);
 
-const table_name = 'Submissions';
+const table_name = "Fall 2019 Slackbot Feedback";
 
-const express = require('express');
-const { WebClient } = require('@slack/web-api');
-const { createEventAdapter } = require('@slack/events-api');
-const { createMessageAdapter } = require('@slack/interactive-messages');
+const express = require("express");
+const { WebClient } = require("@slack/web-api");
+const { createEventAdapter } = require("@slack/events-api");
+const { createMessageAdapter } = require("@slack/interactive-messages");
 
 const slackEvents = createEventAdapter(SLACK_SIGNING_SECRET);
 const slackInteractions = createMessageAdapter(SLACK_SIGNING_SECRET);
@@ -30,127 +30,149 @@ const web = new WebClient(SLACK_BOT_TOKEN);
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use('/slack/events', slackEvents.requestListener());
-app.use('/slack/actions', slackInteractions.requestListener());
+app.use("/slack/events", slackEvents.requestListener());
+app.use("/slack/actions", slackInteractions.requestListener());
 
 app.listen(port, () => {
-    console.log(`Listening for actions/events on port ${port}...`);
-  });
+  console.log(`Listening for actions/events on port ${port}...`);
+});
 
 // https://api.slack.com/events/app_mention
-slackEvents.on('app_mention', (event) => { // bot responds to Slack mentions with survey
+slackEvents.on("app_mention", event => {
+  // bot responds to Slack mentions with survey
 
-    const bot_feedback_message = { // template for message with button to survey
-      token: SLACK_BOT_TOKEN,
-      channel: event.channel, 
-      text: `Hey <@${event.user}>`, 
-      link_names: true,
-      attachments: [
-        {
-          text: 'Would you mind giving us some feedback?',
-          callback_id: 'feedback_form_open',
-          color: '#3149EC',
-          attachment_type: 'default',
-          actions: [
-            {
-              name: 'feedback_button',
-              text: 'Begin Survey!',
-              type: 'button',
-              value: 'feedback'
-            }
-          ]
-        }
-      ]
-    };
-
-    (async () => {
-        // https://api.slack.com/methods/chat.postMessage
-        const res = await web.chat.postMessage(bot_feedback_message)
-          .catch(err => {
-            console.log(err);
-          });
-      })();
-
-  });
-
-slackInteractions.action({type: 'button'}, (payload) => {
-
-    const feedback_dialog = {     // template for dialog with feedback form
-      token: SLACK_BOT_TOKEN,
-      trigger_id: payload.trigger_id,
-      dialog: JSON.stringify({
-        title: 'Feedback Form',
-        callback_id: 'feedback_form_open',
-        submit_label: 'Submit',
-        elements: [
+  const bot_feedback_message = {
+    // template for message with button to survey
+    token: SLACK_BOT_TOKEN,
+    channel: event.channel,
+    text: `Hey <@${event.user}>`,
+    link_names: true,
+    attachments: [
+      {
+        text: "Would you mind giving us some feedback?",
+        callback_id: "feedback_form_open",
+        color: "#3149EC",
+        attachment_type: "default",
+        actions: [
           {
-            label: 'Name',
-            type: 'text',
-            name: 'name',
-            placeholder: 'Enter your name...'
-          }, 
-          {
-            label: 'Rate your experience',
-            type: 'select',
-            name: 'rating',
-            placeholder: 'Rate your experience 1-5...',
-            options: [
-              { label: '1', value: 1 },
-              { label: '2', value: 2 },
-              { label: '3', value: 3 },
-              { label: '4', value: 4 },
-              { label: '5', value: 5 } 
-            ]
-          }, 
-          {
-            label: 'Feedback',
-            type: 'textarea',
-            name: 'feedback',
-            hint: 'Tell us what you thought...'
+            name: "feedback_button",
+            text: "Begin Survey!",
+            type: "button",
+            value: "feedback"
           }
         ]
-      })
-    };
+      }
+    ]
+  };
 
-    (async () => {
-        // https://api.slack.com/methods/dialog.open
-        const res = await web.dialog.open(feedback_dialog)
-          .catch(err => {
-              console.log(err);
-          });
-      })();
+  (async () => {
+    // https://api.slack.com/methods/chat.postMessage
+    const res = await web.chat.postMessage(bot_feedback_message).catch(err => {
+      console.log(err);
+    });
+  })();
+});
 
-  });
+slackInteractions.action({ type: "button" }, payload => {
+  const feedback_dialog = {
+    // template for dialog with feedback form
+    token: SLACK_BOT_TOKEN,
+    trigger_id: payload.trigger_id,
+    dialog: JSON.stringify({
+      title: "Feedback Form",
+      callback_id: "feedback_form_open",
+      submit_label: "Submit",
+      elements: [
+        {
+          label: "The pace of the material so far has been...",
+          type: "select",
+          name: "pace",
+          placeholder: "1-5, 5=too fast, 1=too slow",
+          options: [
+            { label: "1", value: 1 },
+            { label: "2", value: 2 },
+            { label: "3", value: 3 },
+            { label: "4", value: 4 },
+            { label: "5", value: 5 }
+          ]
+        },
+        {
+          label: "My own understanding of the material is...",
+          type: "select",
+          name: "understanding",
+          placeholder: "1-5, 5=I could teach this, 1=I'm totally lost",
+          options: [
+            { label: "1", value: 1 },
+            { label: "2", value: 2 },
+            { label: "3", value: 3 },
+            { label: "4", value: 4 },
+            { label: "5", value: 5 }
+          ]
+        },
+        {
+          label: "How are you enjoying the course?",
+          type: "select",
+          name: "enjoyment",
+          placeholder: "1-5, 5=I’m loving it, 1=I’m hating it",
+          options: [
+            { label: "1", value: 1 },
+            { label: "2", value: 2 },
+            { label: "3", value: 3 },
+            { label: "4", value: 4 },
+            { label: "5", value: 5 }
+          ]
+        },
+        {
+          label: "How can we improve your experience?",
+          type: "textarea",
+          name: "feedback",
+          hint: "Tell us what you think..."
+        }
+      ]
+    })
+  };
 
-slackInteractions.action({type: 'dialog_submission'}, (payload) => {
+  (async () => {
+    // https://api.slack.com/methods/dialog.open
+    const res = await web.dialog.open(feedback_dialog).catch(err => {
+      console.log(err);
+    });
+  })();
+});
 
-    base(table_name).create([ // process the dialog response into Airtable
+slackInteractions.action({ type: "dialog_submission" }, payload => {
+  base(table_name).create(
+    [
+      // process the dialog response into Airtable
       {
         fields: {
-          'Name': payload.user.name, 
-          'Slack Handle': payload.user.id,
-          'Submission': payload.submission.feedback
+          Name: payload.user.id,
+          "Pace Rating": Number(payload.submission.pace),
+          "Understanding Rating": Number(payload.submission.understanding),
+          "Enjoyment Rating": Number(payload.submission.enjoyment),
+          Feedback: payload.submission.feedback
         }
       }
-    ], function(err) {
+    ],
+    function(err) {
       if (err) {
         console.error(err);
       }
+    }
+  );
+
+  bot_response_message = {
+    // template for bot response to completed form
+    token: SLACK_BOT_TOKEN,
+    channel: payload.channel.id,
+    text: `Thanks! <@${payload.user.id}>`,
+    link_names: true
+  };
+
+  (async () => {
+    // https://api.slack.com/methods/chat.postMessage
+    const res = await web.chat.postMessage(bot_response_message).catch(err => {
+      console.log(err);
     });
-
-    bot_response_message = { // template for bot response to completed form
-      token: SLACK_BOT_TOKEN,
-      channel: payload.channel.id, 
-      text: `Thanks! <@${payload.user.id}>`, 
-      link_names: true,
-    };
-
-    (async () => {
-        // https://api.slack.com/methods/chat.postMessage
-        const res = await web.chat.postMessage(bot_response_message)
-          .catch(err => {
-            console.log(err);
-          });
-      })();
-
-  });
+  })();
+});
