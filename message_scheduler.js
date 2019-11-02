bd8f61c9ba8465955d816a7c2b94518a0ff92364
Forrest Feaser <fdf3kz@virginia.edu>
2019-11-02 18:53:21 -0400

    message scheduling for 11/2/2019

--- message_scheduler.js
+++ message_scheduler.js
@@ -6,29 +6,34 @@
  * by Forrest Feaser and Camille Cooper
  * for HackCville, Inc.
  * 
- * 10/23/2019
+ * 11/02/2019
  */
 
 require("dotenv").config();
 
 const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY;
-const AIRTABLE_BASE_ID_DEV = process.env.AIRTABLE_BASE_ID_DEV;
-const TABLE_NAME = "Courses"
-const SLACK_BOT_TOKEN = process.env.SLACK_BOT_TOKEN;
+const AIRTABLE_BASE_ID_PROD = process.env.AIRTABLE_BASE_ID_PROD;
+const SLACK_BOT_TOKEN_HCCOMMUNITY = process.env.SLACK_BOT_TOKEN_HCCOMMUNITY;
+const COURSES_TABLE_NAME = "Courses"
 
 const { WebClient } = require("@slack/web-api");
-const web = new WebClient(SLACK_BOT_TOKEN);
+const web = new WebClient(SLACK_BOT_TOKEN_HCCOMMUNITY);
 
 const Airtable = require("airtable");
-const base = new Airtable({ apiKey: AIRTABLE_API_KEY }).base(AIRTABLE_BASE_ID_DEV);
+const base = new Airtable({ apiKey: AIRTABLE_API_KEY }).base(AIRTABLE_BASE_ID_PROD);
 
-scheduleMessages = () => {
+//for staggering messages, but doesn't work with async
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
+const scheduleMessages = async () => {
 
   const courses = [];
 
-  base(TABLE_NAME)
+  await base(COURSES_TABLE_NAME)
     .select({
-      fields: ["Course Title", "Slack ID", "Survey Datetime"], 
+      fields: ["Course Title", "Slack ID", "Survey Datetime", "Meeting Day"], 
       filterByFormula: "AND(NOT({Course Title} = ''), NOT({Slack ID} = ''), NOT({Survey Datetime} = ''))"
     })
 
@@ -50,6 +55,7 @@ scheduleMessages = () => {
 
         const course_title = course.course_title;
         const slack_ids = course.student_slack_ids;
+        //watch out for DST
         const message_date = new Date(course.survey_date);
         const message_date_epoch_secs = message_date.getTime() / 1000;
 
@@ -57,15 +63,16 @@ scheduleMessages = () => {
 
           //scheduled message containing link to feedback form
           const scheduled_bot_message = {
-            token: SLACK_BOT_TOKEN,
+            token: SLACK_BOT_TOKEN_HCCOMMUNITY,
             channel: user_id,
             post_at: message_date_epoch_secs,
             link_names: true,
-            as_user: true,
+            as_user: false, //make this true for message to appear in feedbackbot DM
             attachments: [
               {
                 text: `Hey <@${user_id}>, would you mind giving us some feedback on ${course_title}?`,
                 callback_id: "feedback_form_open",
+                color: "#3149EC",
                 attachment_type: "default",
                 actions: [
                   {
@@ -79,22 +86,24 @@ scheduleMessages = () => {
             ]
           };
 
-          (async () => {
+          (async () =>  {
             //https://api.slack.com/methods/chat.scheduleMessage
-            const res = await web.chat.scheduleMessage(scheduled_bot_message)
-              .then(() => {
-                console.log(
-                  `Message scheduled for ${user_id} at ${message_date.toLocaleString()}`
-                );
-              })
-              .catch(err => {
-                console.log(err);
-              });
+            await web.chat.scheduleMessage(scheduled_bot_message)
+            .then(() => {
+              console.log(
+                `Message scheduled for ${user_id} at ${message_date.toLocaleString()}`
+              );
+            })
+            .catch(err => {
+              console.log(err);
+            });
+
           })();
 
         });
 
       });
+
     })
 
     .catch(err => {
